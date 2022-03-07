16d8ebb4a08ec029c05c13fd18997efd6cfc0670
kdy1242 <dy12311229@gmail.com>
2022-03-08 02:30:32 +0900

    #2 Layout XML에서 화면 구성

--- test01/src/main/java/com/example/test01/MainActivity.kt
+++ test01/src/main/java/com/example/test01/MainActivity.kt
@@ -14,30 +14,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val name = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "SeSAC"
-        }
+//        val name = TextView(this).apply {
+//            typeface = Typeface.DEFAULT_BOLD
+//            text = "SeSAC"
+//        }
+//
+//        val image = ImageView(this).also {
+//            it.setImageDrawable(
+//                ContextCompat.getDrawable(
+//                this, R.drawable.img
+//            ))
+//        }
+//        val title = TextView(this).apply {
+//            typeface = Typeface.DEFAULT_BOLD
+//            text = "JetPack 과 Kotlin 을 활용한 Android App 개발"
+//        }
+//
+//        val layout = LinearLayout(this).apply {
+//            orientation = LinearLayout.VERTICAL
+//            gravity = Gravity.CENTER
+//            addView(name, WRAP_CONTENT, WRAP_CONTENT)
+//            addView(image, WRAP_CONTENT, WRAP_CONTENT)
+//            addView(title, WRAP_CONTENT, WRAP_CONTENT)
+//        }
+//        setContentView(layout)
 
-        val image = ImageView(this).also {
-            it.setImageDrawable(
-                ContextCompat.getDrawable(
-                this, R.drawable.img
-            ))
-        }
-        val title = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "JetPack 과 Kotlin 을 활용한 Android App 개발"
-        }
-
-        val layout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-            addView(name, WRAP_CONTENT, WRAP_CONTENT)
-            addView(image, WRAP_CONTENT, WRAP_CONTENT)
-            addView(title, WRAP_CONTENT, WRAP_CONTENT)
-        }
-        setContentView(layout)
-
+        setContentView(R.layout.activity_main)
     }
 }
