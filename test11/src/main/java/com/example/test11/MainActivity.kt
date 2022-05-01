732acc584d4b7c4273b6354c246cdd43533a125f
kdy1242 <dy12311229@gmail.com>
2022-05-01 23:24:26 +0900

    #12 Key Event

---
+++ test11/src/main/java/com/example/test11/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.test11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    var initTime = 0L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if(keyCode === KeyEvent.KEYCODE_BACK) {
+            if(System.currentTimeMillis() - initTime > 3000) {
+                Toast.makeText(this, "종료하려면 한번더 누르세요", Toast.LENGTH_SHORT).show()
+                initTime = System.currentTimeMillis()
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
